8a6b1d9cd6aec8d4fa43e59a313cc96898d3d3cc
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2023-07-11 11:38:40 +0200

    IVA

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import "./App.css";
+import {
+  Box,
+  Card,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 function App() {
+  const [price, setPrice] = useState<string>("100");
+  const [typeValue, setTypeValue] = useState<"con-iva" | "sin-iva">("sin-iva");
+
+  const [taxRate, setTaxRate] = useState("21");
+
+  const precioSinIVA = useMemo(() => {
+    if (typeValue === "sin-iva") return price;
+    else {
+      const sv = (Number(price) * 100) / (100 + Number(taxRate));
+      return sv;
+    }
+  }, [price, taxRate, typeValue]);
+
+  const precioConIVA = useMemo(() => {
+    if (typeValue === "con-iva") return price;
+    else return Number(price) + (Number(price) * Number(taxRate)) / 100;
+  }, [price, taxRate, typeValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={5} maxWidth={300} m="auto">
+      <Typography variant="h4" textAlign="center">
+        Calcular IVA
+      </Typography>
+      <Card elevation={3}>
+        <Stack p={3} spacing={3}>
+          <TextField
+            value={price}
+            variant="outlined"
+            label="PVP"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+
+          <FormControl>
+            <FormLabel>¿Incluye IVA?</FormLabel>
+            <RadioGroup
+              value={typeValue}
+              onChange={(e) => setTypeValue(e.target.value as any)}
+            >
+              <FormControlLabel
+                value="sin-iva"
+                label="No (precio bruto)"
+                control={<Radio />}
+              />
+              <FormControlLabel
+                value="con-iva"
+                label="Sí (precio neto)"
+                control={<Radio />}
+              />
+            </RadioGroup>
+          </FormControl>
+
+          <TextField
+            value={taxRate}
+            variant="outlined"
+            label="IVA"
+            onChange={(e) => setTaxRate(e.target.value)}
+          />
+
+          <Typography variant="h5" textAlign={"center"}>
+            {Number(precioSinIVA).toFixed(2)} + {taxRate}% ={" "}
+            {Number(precioConIVA).toFixed(2)}
+          </Typography>
+        </Stack>
+      </Card>
+    </Box>
   );
 }
 
